fe17fe7484d8197f06f216fe1a8575080b2c92d7
bananabi <131904987+bananabi@users.noreply.github.com>
2025-10-21 13:28:32 -0400

    fixed a issue with the code

--- main.ts
+++ main.ts
@@ -5,30 +5,24 @@
  * This program ...
 */
 
-// setup
-basic.clearScreen()
-basic.showIcon
+//variables
+let number1 = 0
+let number2 = 0
 
-// variable
-let number1: number = 0
+// random number generate
 number1 = randint(0, 99)
-let number2: number = 0
 number2 = randint(0, 99)
 
-// button A
-input.onButtonPressed(Button.A, function() {
+// show message at the start
+basic.showIcon(IconNames.Happy)
+
+// when A is pressed start the program 
+input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
-    basic.showString('#1 :')
+    basic.pause(500)
+    basic.showString("#1")
     basic.showNumber(number1)
-
-})
-
-// button B
-input.onButtonPressed(Button.B, function() {
-    basic.clearScreen()
-    basic.showString('#2 :')
-    basic.showNumber(number2)
-    
+    basic.showIcon(IconNames.Happy)
 })
 
 // when B is pressed do the same as A 
@@ -40,7 +34,7 @@ input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Happy)
 })
 
-// compare the two numbers
+// when the microbit is shaken compare the two numbers
 input.onGesture(Gesture.Shake, function () {
     basic.clearScreen()
     if (number1 < number2) {
